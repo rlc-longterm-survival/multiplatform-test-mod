087464c60b7b4b250b72d124291e83ca94c08f9a
yezhiyi9670 <yezhiyi9670@sparks-lab.art>
2023-09-06 16:26:18 +0800

    Initial commit

---
+++ common/src/main/java/yezhiyi9670/mptest/ExampleMod.java
@@ -0,0 +1,13 @@
+package yezhiyi9670.mptest;
+
+import com.google.common.base.Suppliers;
+
+import java.util.function.Supplier;
+
+public class ExampleMod {
+    public static final String MOD_ID = "mptest";
+
+    public static void init() {
+
+    }
+}
